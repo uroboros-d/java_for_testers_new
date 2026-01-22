c580ea90e602ab34e465a51caafab229970c98cd
Dzmitry Shablinski <tassap2002@gmail.com>
2026-01-22 17:37:36 +0300

    создал класс Geometry с переменными

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,10 @@
+public class Geometry {
+    public static void main(String[] args) {
+        System.out.println("Площадь квадрата со стороной " + 2 + " = " + (2*2));
+
+        var squareSide = 7.;
+        // var - переменная, а тип double
+        //чтобы показать джаве, что число должно быть не int, а double, можно написать 7.0 или просто 7.
+        System.out.println("Площадь квадрата со стороной " + squareSide + " = " + (squareSide * squareSide));
+    }
+}
