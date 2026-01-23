011efc0136e26b6c6d3ecbc6219b6ac597e6e68e
Dzmitry Shablinski <tassap2002@gmail.com>
2026-01-23 13:41:59 +0300

    эксперименты с функ (лекция 1.4)

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -1,10 +1,22 @@
 public class Geometry {
     public static void main(String[] args) {
-        System.out.println("Площадь квадрата со стороной " + 2 + " = " + (2*2));
+        printSquareArea(5);
+        printRectangleArea(3,4);
+    }
 
-        var squareSide = 7.;
-        // var - переменная, а тип double
-        //чтобы показать джаве, что число должно быть не int, а double, можно написать 7.0 или просто 7.
-        System.out.println("Площадь квадрата со стороной " + squareSide + " = " + (squareSide * squareSide));
+    private static void printRectangleArea(double firstSide, double secondSide) {
+        System.out.println("Площадь прямоугольника со сторонами " + firstSide + " и " + secondSide + " = " + getRectangleArea(firstSide, secondSide));
+    }
+
+    private static double getRectangleArea(double firstSide, double secondSide) {
+        return firstSide * secondSide;
+    }
+
+    static void printSquareArea(double side) {
+        System.out.println("Площадь квадрата со стороной " + side + " = " + getSquareArea(side));
+    }
+
+    private static double getSquareArea(double side) {
+        return side * side;
     }
 }
