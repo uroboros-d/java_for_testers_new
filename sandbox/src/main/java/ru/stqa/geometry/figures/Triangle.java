d3a4e6af1cb26cd02c9c3c907835884e9de343bb
Dzmitry Shablinski <tassap2002@gmail.com>
2026-01-27 12:21:50 +0300

    созданы новые тесты (Задание №3)

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -2,13 +2,23 @@ package ru.stqa.geometry.figures;
 
 public record Triangle(double sideOne, double sideTwo, double sideThree) {
 
+    public  Triangle {
+        if (sideOne < 0 || sideTwo < 0 || sideThree < 0) {
+            throw new IllegalArgumentException("Triangle side must be non-negative");
+        }
+        if (sideOne + sideTwo < sideThree ||
+                sideOne + sideThree < sideTwo ||
+                sideTwo + sideThree < sideOne) {
+            throw new IllegalArgumentException("The sum of any two triangle sides must not be less than the third triangle side");
+        }
+    }
     public double getPerimeter() {
         return this.sideOne + this.sideTwo + this.sideThree;
     }
 
     public double getArea() {
 
-        var p = (this.sideOne + this.sideTwo + this.sideThree)/2;
+        var p = (this.sideOne + this.sideTwo + this.sideThree)/2; //p - semiperimeter
         return Math.sqrt(p*(p-this.sideOne)*(p-this.sideTwo)*(p-this.sideThree));
     }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -23,4 +23,81 @@ public class TriangleTests {
         Assertions.assertEquals(12.0, new Triangle(3., 4., 5.).getPerimeter());
     }
 
+    @Test
+        // тест для нарушения неравенства треугольника (сумма сторон Один и Два < стороны Три)
+    void cannotCreateTriangleWithTriangleInequalityViolationSideThree(){
+        try {
+            new Triangle(2, 3,6);
+            // если мы прошли строку кода выше, значит поведение не соответствует ожидаемому (ожидается исключение) и
+            // тест должен упасть
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест выполняется успешно, при нарушении неравенства треугольника бросая исключение - выполняя блок catch");
+        }
+    }
+
+    @Test
+        // тест для нарушения неравенства треугольника (сумма сторон Один и Три < стороны Два)
+    void cannotCreateTriangleWithTriangleInequalityViolationSideTwo(){
+        try {
+            new Triangle(2, 7,4);
+            // если мы прошли строку кода выше, значит поведение не соответствует ожидаемому (ожидается исключение) и
+            // тест должен упасть
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест выполняется успешно, при нарушении неравенства треугольника бросая исключение - выполняя блок catch");
+        }
+    }
+
+    @Test
+        // тест для нарушения неравенства треугольника (сумма сторон Два и Три < стороны Один)
+    void cannotCreateTriangleWithTriangleInequalityViolationSideOne(){
+        try {
+            new Triangle(8, 3,4);
+            // если мы прошли строку кода выше, значит поведение не соответствует ожидаемому (ожидается исключение) и
+            // тест должен упасть
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест выполняется успешно, при нарушении неравенства треугольника бросая исключение - выполняя блок catch");
+        }
+    }
+
+    @Test
+        // тест для стороны Один треугольника = отрицательное число
+    void cannotCreateTriangleWithNegativeSideOne(){
+        try {
+            new Triangle(-5, 4,1);
+            // если мы прошли строку кода выше, значит поведение не соответствует ожидаемому (ожидается исключение) и
+            // тест должен упасть
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест выполняется успешно, при отрицательном значении длины стороны бросая исключение - выполняя блок catch");
+        }
+    }
+
+    @Test
+        // тест для стороны Два треугольника = отрицательное число
+    void cannotCreateTriangleWithNegativeSideTwo(){
+        try {
+            new Triangle(5, -4,1);
+            // если мы прошли строку кода выше, значит поведение не соответствует ожидаемому (ожидается исключение) и
+            // тест должен упасть
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест выполняется успешно, при отрицательном значении длины стороны бросая исключение - выполняя блок catch");
+        }
+    }
+
+    @Test
+        // тест для стороны Три треугольника = отрицательное число
+    void cannotCreateTriangleWithNegativeSideThree(){
+        try {
+            new Triangle(5, 4,-1);
+            // если мы прошли строку кода выше, значит поведение не соответствует ожидаемому (ожидается исключение) и
+            // тест должен упасть
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Тест выполняется успешно, при отрицательном значении длины стороны бросая исключение - выполняя блок catch");
+        }
+    }
 }
