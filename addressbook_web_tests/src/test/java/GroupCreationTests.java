775465be469f1c9a5e1e0bd09457b1c01a8f5c46
Dzmitry Shablinski <tassap2002@gmail.com>
2026-01-30 15:31:20 +0300

    создан canRemoveGroup (Лекция 2.7)

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -64,4 +64,25 @@ public class GroupCreationTests {
         driver.findElement(By.name("submit")).click();
         driver.findElement(By.linkText("group page")).click();
     }
+
+    @Test
+    public void canRemoveGroup() {
+        if (! isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+        if (! isElementPresent(By.name("selected[]"))) {
+            driver.findElement(By.name("new")).click();
+            driver.findElement(By.name("group_name")).click();
+            driver.findElement(By.name("group_name")).sendKeys("group two");
+            driver.findElement(By.name("group_header")).click();
+            driver.findElement(By.name("group_header")).sendKeys("group two header");
+            driver.findElement(By.name("group_footer")).click();
+            driver.findElement(By.name("group_footer")).sendKeys("group two footer");
+            driver.findElement(By.name("submit")).click();
+            driver.findElement(By.linkText("group page")).click();
+        }
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("groups")).click();
+    }
 }
