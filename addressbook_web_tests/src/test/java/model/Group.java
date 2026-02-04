75e2355172a7ea2268f227225fe5e3560a1c4ce8
Dzmitry Shablinski <tassap2002@gmail.com>
2026-02-04 10:57:02 +0300

    небольшая коррекция методов выше

--- addressbook_web_tests/src/test/java/model/Group.java
+++ addressbook_web_tests/src/test/java/model/Group.java
@@ -13,12 +13,12 @@ public record Group(String name, String header, String footer) {
     }
 
     public Group withHeader(String header) {
-        //вернуть объект с другим именем, а хэдер и футер такие же как у существующего объекта
+        //вернуть объект с другим хэдером, а имя и футер такие же как у существующего объекта
         return new Group(this.name, header, this.footer);
     }
 
     public Group withFooter(String footer) {
-        //вернуть объект с другим именем, а хэдер и футер такие же как у существующего объекта
+        //вернуть объект с другим футером, а имя и хэдер такие же как у существующего объекта
         return new Group(this.name, this.header, footer);
     }
 }
