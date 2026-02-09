f22a8a8d9b13c98140707d323e3ae8bfe5d45c39
Dzmitry Shablinski <tassap2002@gmail.com>
2026-02-09 10:28:52 +0300

    изменен тест canCreateGroup()-добавлены @ParameterizedTest,@ValueSource(Лекция 4.5)

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -3,13 +3,16 @@ package tests;
 import model.Group;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void canCreateGroup() {
+    @ParameterizedTest
+    @ValueSource(strings = {"group name", "group name'"})
+    public void canCreateGroup(String name) {
         int groupCount = app.groups().getCount();
-        app.groups().createGroup(new Group("group name/header/footer", "group header", "group footer"));
+        app.groups().createGroup(new Group(name, "group header", "group footer"));
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount+1, newGroupCount);
     }
