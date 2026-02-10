24697cef2c5458a52306981cd2a090d42bdc3618
Dzmitry Shablinski <tassap2002@gmail.com>
2026-02-10 17:21:49 +0300

    в groupProvider() добавлены withName(),withHeader(),withFooter()(Лекция 4.6)

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -21,7 +21,7 @@ public class GroupCreationTests extends TestBase {
             }
         }
         for (int i = 0; i < 5; i++) {
-            result.add(new Group("", randomString(i*10), randomString(i*10), randomString(i*10)));
+            result.add(new Group().withName(randomString(i*10)).withHeader(randomString(i*10)).withFooter(randomString(i*10)));
         }
         return result;
     }
